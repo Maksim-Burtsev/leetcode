df76b0e7681e714441f178c9a4edc86b59ff80d2
MaximBurtsev <zadrot-lol@list.ru>
2022-07-20 13:21:17 +0300

    addTwoNumbers

---
+++ addTwoNumbers.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+
+        pre_head = node = ListNode()
+        carry = 0
+
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+
+            carry, val = carry // 10, carry % 10
+
+            node.next = ListNode(val=val)
+            node = node.next
+
+        return pre_head.next
